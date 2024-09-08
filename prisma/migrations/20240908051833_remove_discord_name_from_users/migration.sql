c379bdf8da43896fa61a107bff0649aaca013d23
Tyler Hogan <tybot204@gmail.com>
2024-09-07 22:20:03 -0700

    Remove discordName from users as it is unused

---
+++ prisma/migrations/20240908051833_remove_discord_name_from_users/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `discord_name` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "discord_name";
