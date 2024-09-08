a6333d6dbbab2068f0f39bdbafa0f31de2610b4b
Tyler Hogan <tybot204@gmail.com>
2024-09-07 22:23:26 -0700

    Remove discordName from users as it is unused (#15)

--- src/util/registerUser.ts
+++ src/util/registerUser.ts
@@ -6,5 +6,5 @@ export const registerUser = async (user: User) => {
   const existingUser = await prisma.user.findUnique({ where: { discordId: user.id } });
   if (existingUser) return existingUser;
 
-  return await prisma.user.create({ data: { discordId: user.id, discordName: user.displayName } });
+  return await prisma.user.create({ data: { discordId: user.id } });
 };
