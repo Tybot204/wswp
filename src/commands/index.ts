763477e03de9589478322e3fcaaf49e51702ea1b
Caleb <calebmh1@gmail.com>
2024-09-08 22:59:05 -0500

    New rate single game command for rating or re-rating individual games.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,6 +3,7 @@ import { Command } from "..";
 import { addGame } from "./addGame";
 import { listGames } from "./listGames";
 import { rateGames } from "./rateGames";
+import { rateSingleGame } from "./rateSingleGame";
 import { removeGame } from "./removeGame";
 import { whatShouldWePlay } from "./whatShouldWePlay";
 
@@ -10,6 +11,7 @@ export const commandMap: { [key: string]: Command } = {
   addgame: addGame,
   listgames: listGames,
   rategames: rateGames,
+  ratesinglegame: rateSingleGame,
   removegame: removeGame,
   whatshouldweplay: whatShouldWePlay,
 };
