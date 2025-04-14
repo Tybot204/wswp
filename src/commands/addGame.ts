ed83bbd08cfb981a6f0a43f0b105e11e3a9e3584
Tyler Hogan <tybot204@gmail.com>
2025-04-13 23:30:52 -0700

    Remove deprecated ephemeral calls in Discord replies

--- src/commands/addGame.ts
+++ src/commands/addGame.ts
@@ -1,4 +1,4 @@
-import { InteractionContextType, SlashCommandBuilder } from "discord.js";
+import { InteractionContextType, MessageFlags, SlashCommandBuilder } from "discord.js";
 
 import { Command, prisma, steam } from "..";
 
@@ -60,7 +60,7 @@ export const addGame: Command = {
 
     const guildId = interaction.guildId;
     if (!guildId) {
-      await interaction.reply({ content: "This command can only be used in a server.", ephemeral: true });
+      await interaction.reply({ content: "This command can only be used in a server.", flags: MessageFlags.Ephemeral });
       return;
     }
 
