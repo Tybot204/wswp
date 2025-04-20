b9e6c78fde856f89571adfbdbfe23e59f2b7c4a4
Tyler Hogan <tybot204@gmail.com>
2025-04-20 01:14:52 -0700

    Truncate long ratings to 2 decimal precision

--- src/commands/whatShouldWePlay.ts
+++ src/commands/whatShouldWePlay.ts
@@ -84,7 +84,7 @@ export const whatShouldWePlay: Command = {
 
     const fields: APIEmbedField[] = [];
     if (game.gameURL) fields.push({ name: "URL:", value: game.gameURL });
-    fields.push({ inline: true, name: "Rating (Average)", value: totalRatings[0]._avg.score?.toString() });
+    fields.push({ inline: true, name: "Rating (Average)", value: totalRatings[0]._avg.score?.toFixed(2) });
     fields.push({ inline: true, name: "Free?", value: game.free ? "Yes" : "No" });
     fields.push({ inline: true, name: "Players", value: `${game.minPlayers} - ${game.maxPlayers}` });
 
