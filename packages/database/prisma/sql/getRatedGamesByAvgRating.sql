045bdb9e317448e09344eefc49d48518f37b773d
Tyler Hogan <tybot204@gmail.com>
2026-01-19 18:10:32 -0800

    Add ability to skip through WSWP results

--- packages/database/prisma/sql/getRatedGamesByAvgRating.sql
+++ packages/database/prisma/sql/getRatedGamesByAvgRating.sql
@@ -1,16 +1,16 @@
 -- @param {String} $1:guildId Guild ID to filter games by
--- @param {Int} $3:page Page number for pagination (5 per page)
+-- @param {Int} $3:page Page number for pagination (5 per page + 1 pagination)
 SELECT *
 FROM games
 INNER JOIN (
-  SELECT game_id, AVG(score) AS avg_score, COUNT(DISTINCT ratings.user_id) AS user_count
+  SELECT game_id, AVG(score) AS avg_score
   FROM ratings
   INNER JOIN games ON games.id = ratings.game_id
   INNER JOIN users ON users.id = ratings.user_id
   WHERE games.guild_id = $1
   AND users.discord_id = ANY($2)
   GROUP BY game_id
-  HAVING COUNT(DISTINCT ratings.user_id) = array_length($2::text[], 1)
+  HAVING COUNT(DISTINCT ratings.user_id) = ARRAY_LENGTH($2::text[], 1)
 ) avg_ratings ON avg_ratings.game_id = games.id
 WHERE games.guild_id = $1
 ORDER BY COALESCE(avg_score, 0) DESC
